f469542dfa7c4d62b999d18290425e7c90a806e0
P0lid0ri <gabrielescuola9910@gmail.com>
2024-11-26 12:08:43 +0100

    first commit

---
+++ src/main/java/com/mycompany/torneo/Torneo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.torneo;
+
+/**
+ *
+ * @author GABRIELE
+ */
+public class Torneo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
